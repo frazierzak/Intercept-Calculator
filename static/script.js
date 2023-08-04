279209cae82b7ef72a43078c4b4b8e543ba8f9b6
frazierzak <zakfrazier@gmail.com>
2023-08-04 11:33:05 -0400

    Fixed desiredTime calculation

--- static/script.js
+++ static/script.js
@@ -17,41 +17,91 @@ document.addEventListener('DOMContentLoaded', function () {
       let targetDistance = parseFloat(
         document.getElementById('distance_target').value
       )
-
-      let interceptCourse = calculateInterceptCourse(
-        targetBearing,
-        targetHeading,
-        targetSpeed,
-        maxSpeed
+      let desiredTimeToIntercept = parseFloat(
+        document.getElementById('desired_time').value
       )
 
-      let interceptCourseRad = interceptCourse * (Math.PI / 180)
-      let targetBearingRad = targetBearing * (Math.PI / 180)
-      let targetHeadingRad = targetHeading * (Math.PI / 180)
+      let interceptCourse, timeToIntercept, distanceToIntercept, requiredSpeed
 
-      let closingSpeedX =
-        maxSpeed * Math.cos(interceptCourseRad) -
-        targetSpeed * Math.cos(targetHeadingRad)
-      let closingSpeedY =
-        maxSpeed * Math.sin(interceptCourseRad) -
-        targetSpeed * Math.sin(targetHeadingRad)
-      let closingSpeed = Math.sqrt(
-        closingSpeedX * closingSpeedX + closingSpeedY * closingSpeedY
-      )
+      if (!isNaN(desiredTimeToIntercept)) {
+        // Convert all angles to radians for calculations
+        targetBearing = targetBearing * (Math.PI / 180)
+        targetHeading = targetHeading * (Math.PI / 180)
 
-      let timeToIntercept = targetDistance / closingSpeed
-      let distanceToIntercept = timeToIntercept * maxSpeed
+        // Calculate the new position of the target after desiredTimeToIntercept
+        let newTargetX =
+          targetDistance * Math.cos(targetBearing) +
+          targetSpeed * desiredTimeToIntercept * Math.cos(targetHeading)
+        let newTargetY =
+          targetDistance * Math.sin(targetBearing) +
+          targetSpeed * desiredTimeToIntercept * Math.sin(targetHeading)
+
+        // Calculate the distance to the new target position
+        distanceToIntercept = Math.sqrt(
+          newTargetX * newTargetX + newTargetY * newTargetY
+        )
+        requiredSpeed = distanceToIntercept / desiredTimeToIntercept
+
+        if (requiredSpeed > maxSpeed) {
+          document.getElementById(
+            'resultsContainer'
+          ).innerHTML = `<p>Interception is not possible within the given timeframe with current maximum speed.</p>`
+          return
+        }
+
+        // Calculate the intercept course to the new target position
+        interceptCourse = Math.atan2(newTargetY, newTargetX)
+
+        // Convert the intercept course back to degrees
+        interceptCourse = interceptCourse * (180 / Math.PI)
+
+        // Normalize the heading to the range [0, 360)
+        while (interceptCourse < 0) {
+          interceptCourse += 360
+        }
+        while (interceptCourse >= 360) {
+          interceptCourse -= 360
+        }
+
+        timeToIntercept = desiredTimeToIntercept
+      } else {
+        interceptCourse = calculateInterceptCourse(
+          targetBearing,
+          targetHeading,
+          targetSpeed,
+          maxSpeed
+        )
+
+        let interceptCourseRad = interceptCourse * (Math.PI / 180)
+        let targetBearingRad = targetBearing * (Math.PI / 180)
+        let targetHeadingRad = targetHeading * (Math.PI / 180)
+
+        let closingSpeedX =
+          maxSpeed * Math.cos(interceptCourseRad) -
+          targetSpeed * Math.cos(targetHeadingRad)
+        let closingSpeedY =
+          maxSpeed * Math.sin(interceptCourseRad) -
+          targetSpeed * Math.sin(targetHeadingRad)
+        let closingSpeed = Math.sqrt(
+          closingSpeedX * closingSpeedX + closingSpeedY * closingSpeedY
+        )
+
+        timeToIntercept = targetDistance / closingSpeed
+        distanceToIntercept = timeToIntercept * maxSpeed
+        requiredSpeed = distanceToIntercept / timeToIntercept
+      }
 
       // Display results in the resultsContainer
       document.getElementById(
         'resultsContainer'
-      ).innerHTML = `<p>Intercept Course: <span>${interceptCourse.toFixed(
-        2
+      ).innerHTML = `<p>Intercept Course: <span>${Math.round(
+        interceptCourse
       )}°</span></p>
-    <p>Time to Intercept: <span>${timeToIntercept.toFixed(2)} HR</span></p>
-    <p>Distance to Intercept: <span>${distanceToIntercept.toFixed(
-      2
-    )} KM</span></p>`
+    <p>Time to Intercept: <span>${timeToIntercept.toFixed(1)} HR</span></p>
+    <p>Distance to Intercept: <span>${Math.round(
+      distanceToIntercept
+    )} KM</span></p>
+    <p>Speed Required: <span>${Math.round(requiredSpeed)} KM/HR</span></p>`
     })
 })
 
